fe8b13b89c5a134f89f571b8be29cb5cc6bb558a
ErlanAyapov <simsiti0009ggg@gmail.com>
2021-08-09 10:53:12 +0600

    fix errors

--- Task 4/a_n.cpp
+++ Task 4/a_n.cpp
@@ -3,24 +3,23 @@
 using namespace std;
 
 int main() {
-	short int a, n, d, res = 0;
+	short int a, n, d, res;
 	unsigned short int i;
 	cout << "a, n engis: "; cin >> a >> d;
 	if (d <= -1) {
 		n = d / -1;
-	}
-	
-	for (i = 1; i <= n; i++) {
-		res = res + a;
-		cout << a << " + ";
-	}
-	if (d <= -1 && a >= 1) {
-		cout << "= -" << res << endl;
+		for (i = 1; i <= n; i++) {
+			res = res + a;
+			cout << a << " + ";
+		}
 	} else {
-		cout << "= " << res << endl;
+		for (i = 1; i <= d; i++) {
+			res = res + a;
+			cout << a << " + ";
+		}
 	}
 	
-	
+	cout << "= " << res << endl;
 	
 	return 0;
 }
